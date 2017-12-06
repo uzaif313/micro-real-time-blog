ffe2d8e6043da22d09d661525c02f422e1be593c
Uzaif <uzaif313@gmail.com>
2017-12-06 11:01:57 +0530

    set express middleware

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require("express")
-const morgon = require('morgon');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const hbs = require("hbs");
 const expressHbs = require("express-handlebars");
 
+const port = 8000;
 const app = express();
+app.engine('.hbs',expressHbs({defaultLayout:'application',extname:".hbs"}));
+
+app.set('view engine','hbs');
+
+app.use(express.static(__dirname+"/public"));
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.listen(port,(err)=>{
+  if(err){
+    console.log(err)
+  }else{
+    console.log(`serve is bootup on ${port}`)
+  }
+})
