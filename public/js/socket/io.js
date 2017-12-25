95dacfd38d25c87fc08301bbc7f698f4933b6be8
Uzaif <uzaif313@gmail.com>
2017-12-25 22:32:45 +0530

    client side socket io emit event for tweet

--- public/js/socket/io.js
+++ public/js/socket/io.js
@@ -1,4 +1,17 @@
 $(function() {
-    var socket = io();
-     
+  var socket = io();
+
+  // handle tweet form submit event
+  $('#tweet_it').on('submit',handleSubmit)
+
+  /**
+  * callback for handle submit event
+  * @param {Object} e for handle jquery Event
+  **/
+  function handleSubmit(e) {
+    _content = $('#tweet').val();
+    socket.emit('tweet', {content: _content})
+    $('#tweet').val('')
+    e.preventDefault();
+  }
 })
