794fefe3ff80546bb049c8158a529bd1f0c31a1c
Uzaif <uzaif313@gmail.com>
2017-12-25 22:32:20 +0530

    server side socket io listing add for tweet

--- socket/io.js
+++ socket/io.js
@@ -1,5 +1,11 @@
 module.exports = function(io) {
   io.on('connection', function(socket) {
     console.log('Just connected');
+    const user = socket.request.user;
+    console.log(user.name)
+
+    socket.on('tweet', (data)=>{
+      console.log(data)
+    })
   });
 };
