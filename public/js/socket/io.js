ae58ec937aa6057f5eb9a7975b630e33dda98ae9
uzaif <uzaif313@gmail.com>
2017-12-28 07:49:40 +0530

    integrate client side listing feature for all user

--- public/js/socket/io.js
+++ public/js/socket/io.js
@@ -4,6 +4,13 @@ $(function() {
   // handle tweet form submit event
   $('#tweet_it').on('submit',handleSubmit)
 
+  //  Broadcast tweet to all user via listing on inComingTweets listner
+
+  socket.on("inComingTweets", function(data){
+    console.log(data)
+  })
+
+
   /**
   * callback for handle submit event
   * @param {Object} e for handle jquery Event
